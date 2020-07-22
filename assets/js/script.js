bfc5cffe5cc21222490ccd8525552f27a8e62eb2
Dammyton <owolabidamilola98@gmail.com>
2020-07-22 23:07:06 +0100

    added the files

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+//RECENT WORK CAROUSEL
+new Glider(document.querySelector('.glider'), {
+    slidesToShow: 6,
+    slidesToScroll: 6,
+    draggable: true,
+    dots: '.dot',
+    arrows: {
+      prev: '.glider-prev',
+      next: '.glider-next'
+    }
+  })
+  
+  // Initialize AOS
+  AOS.init({
+    duration: 1200,
+  });
+
+    // Welcome Text Typewriting
+    var i = 0;
+    var txt = 'I’m a beauty, lifestyle portrait photographer based in Queens, New York.';
+    var speed = 150;
+    
+    function typeWriter() {
+      if (i < txt.length) {
+        document.getElementById("first-text").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+      }
+    }
