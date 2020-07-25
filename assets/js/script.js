5b69073b0d399e229cab93a8e917b54eeea68545
Dammyton <owolabidamilola98@gmail.com>
2020-07-25 09:20:23 +0100

    added the files

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,9 +7,39 @@ new Glider(document.querySelector('.glider'), {
     arrows: {
       prev: '.glider-prev',
       next: '.glider-next'
-    }
+    },
   })
-  
+
+  const glider = new Glider(document.getElementById('glider'));
+
+function sliderAuto(slider, miliseconds) {
+ const slidesCount = slider.track.childElementCount;
+ let slideTimeout = null;
+ let nextIndex = 1;
+
+ function slide () {
+   slideTimeout = setTimeout(
+     function () {
+       if (nextIndex >= slidesCount ) {
+         nextIndex = 0;
+       }
+       slider.scrollItem(nextIndex++);
+     },
+     miliseconds
+   );
+ }
+
+ slider.ele.addEventListener('glider-animated', function() {
+   window.clearInterval(slideTimeout);
+   slide();
+ });
+
+ slide();
+}
+
+sliderAuto(glider, 1000)
+
+
   //FOR BURGER NAVIGATION
 $(document).ready(function(){
   $('.burger').on('click', function(){
